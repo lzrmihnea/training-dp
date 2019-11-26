c840db380f25339a33f566e6bc1c776dc5b47580
mihnea.lazar <mihnea.lazar@accesa.eu>
2019-11-26 17:27:03 +0200

    [NoRef-0] Initial project files added

---
+++ src/main/java/eu/training/dp/TrainingDpApplication.java
@@ -0,0 +1,13 @@
+package eu.training.dp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingDpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingDpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/training/dp/TrainingDpApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.training.dp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingDpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
